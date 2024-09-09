9d6a57a2a4ab116d9402e8709a98b47d56436f53
zaritsu <zaritsu030907@gmail.com>
2024-09-09 14:44:49 +0800

    init

---
+++ physical/src/test/java/pr/physical/PhysicalApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.physical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
