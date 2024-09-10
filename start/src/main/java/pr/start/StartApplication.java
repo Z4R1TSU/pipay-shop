d8fcddc1eb8e1e57171b9a2a1b339f8dd44189a6
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-10 15:02:50 +0800

    配置启动类，添加其他模块到启动类

--- start/src/main/java/pr/start/StartApplication.java
+++ start/src/main/java/pr/start/StartApplication.java
@@ -2,8 +2,12 @@ package pr.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pr", "pr.common", "pr.user", "pr.chat", "pr.online", "pr.physical", "pr.hotel"}) // 包扫描
+@EnableJpaRepositories(basePackages = {"pr", "pr.common", "pr.user", "pr.chat", "pr.online", "pr.physical", "pr.hotel"})
+@EntityScan(basePackages = {"pr", "pr.common", "pr.user", "pr.chat", "pr.online", "pr.physical", "pr.hotel"})
 public class StartApplication {
 
 	public static void main(String[] args) {
