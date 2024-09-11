40d200d492070b3239c01bd212b1afefd29d3724
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-11 14:58:03 +0800

    创建SwaggerConfig配置文件

---
+++ common/src/main/java/pr/common/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package pr.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    // TODO: Swagger其实可要可不要，apifox基本可以替代
+
+}
