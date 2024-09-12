752ad9a0cefc517bb4788433de5c85828822db4c
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-12 11:24:21 +0800

    修改common模块的application.yml文件，使得common模块的配置文件可以被其他模块继承。但仍需进一步配置其他模块

--- common/src/main/java/pr/common/config/CommonPropertyConfig.java
+++ common/src/main/java/pr/common/config/CommonPropertyConfig.java
@@ -1,4 +1,10 @@
 package pr.common.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.yml")
 public class CommonPropertyConfig {
+    // 如果需要，可以在这里添加一些通用的bean定义
 }
