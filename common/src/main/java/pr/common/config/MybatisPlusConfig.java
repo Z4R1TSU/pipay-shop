9b1ed3e91cc9609b68bb0279ceb2c49b4116860c
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-13 12:33:01 +0800

    更新MP配置文件

--- common/src/main/java/pr/common/config/MybatisPlusConfig.java
+++ common/src/main/java/pr/common/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package pr.common.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,8 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        // TODO: 可以修改需要的数据库类型
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 
