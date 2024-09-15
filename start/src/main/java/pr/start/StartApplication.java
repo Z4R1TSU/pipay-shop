7ecdff6a63dff472f052ced4ab0469e42ce84a4d
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-15 12:18:27 +0800

    完成application.yml的配置

--- start/src/main/java/pr/start/StartApplication.java
+++ start/src/main/java/pr/start/StartApplication.java
@@ -3,11 +3,15 @@ package pr.start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "pr")
 @EnableJpaRepositories(basePackages = "pr")
 @EntityScan(basePackages = "pr")
+@EnableConfigurationProperties
+@PropertySource("classpath:application.yml")
 public class StartApplication {
 
 	public static void main(String[] args) {
