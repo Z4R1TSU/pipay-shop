c5361c8558a179a821e379cdb983af944d7c6be8
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-18 23:07:31 +0800

    添加常量用户登录token

--- common/src/main/java/pr/common/util/Constants.java
+++ common/src/main/java/pr/common/util/Constants.java
@@ -1,4 +1,10 @@
 package pr.common.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Constants {
+
+    public static final String REDIS_LOGIN_TOKEN_PREFIX = "login_token_";
+
 }
