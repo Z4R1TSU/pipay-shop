6bcbd9a8bb5e0a9f8efa52b25de212710900dd25
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-18 23:11:47 +0800

    添加常量token在redis的过期时间

--- common/src/main/java/pr/common/util/Constants.java
+++ common/src/main/java/pr/common/util/Constants.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Component;
 public class Constants {
 
     public static final String REDIS_LOGIN_TOKEN_PREFIX = "login_token_";
+    
+    public static final Long REDIS_TOKEN_EXPIRE_TIME = 8 * 60 * 60 * 1000L; // 8 hours
 
 }
