be2969312b823078252e6151c1f889fc114da4a8
Zari_Tsu <zaritsu030907@gmail.com>
2024-09-19 11:25:51 +0800

    添加验证码前缀到常量工具类

--- common/src/main/java/pr/common/util/Constants.java
+++ common/src/main/java/pr/common/util/Constants.java
@@ -7,6 +7,10 @@ public class Constants {
 
     public static final String REDIS_LOGIN_TOKEN_PREFIX = "login_token_";
     
+    public static final String REDIS_CAPTCHA_PREFIX = "captcha_";
+    
     public static final Long REDIS_TOKEN_EXPIRE_TIME = 8 * 60 * 60 * 1000L; // 8 hours
+    
+    public static final Long REDIS_CAPTCHA_EXPIRE_TIME = 2 * 60 * 1000L; // 2 minutes
 
 }
